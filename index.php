6baacc7a649fdbde3c866184e850c3f11c08b42a
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-01-11 15:28:31 +0530

    Added getCurrentUri method

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php
     require_once 'vendor/autoload.php';
-
     use Chahal26\PhpSimpleRouter\Router;
-    $router = new Router();
 
-    print_r($router);
+    $router = new Router();
--- src/Router.php
+++ src/Router.php
@@ -1,6 +1,22 @@
 <?php
-    namespace Chahal26\PhpSimpleRouter;
+namespace Chahal26\PhpSimpleRouter;
 
-    Class Router {
-        
+Class Router 
+{
+    /**
+     * Get Current Request URI
+     */
+    public function getCurrentUri():string
+    {
+        $request_uri = rawurldecode($_SERVER['REQUEST_URI']);
+        $uri = substr($request_uri, strlen(implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/'));
+
+        if (strstr($uri, '?')) {
+            $uri = substr($uri, 0, strpos($uri, '?'));
+        }
+
+        $uri = '/'. trim($uri, '/');
+
+        return $uri;
     }
+}
