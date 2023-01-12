5fa17bdbf266ebfb9fe071ec6c679bc3ad0aa836
Sahil Chahal <dev.sahilchahal@gmail.com>
2023-01-12 12:23:06 +0530

    Demo File

--- index.php
+++ index.php
@@ -2,4 +2,19 @@
     require_once 'vendor/autoload.php';
     use Chahal26\PhpSimpleRouter\Router;
 
+    /* Creating Route Instance */
     $router = new Router();
+
+    /* Defining Routes */
+    $router->get('/', function(){
+        echo "<h1>Welcome To Home Page</h1>";
+    });
+    $router->get('register', function(){
+        echo "<h1>Welcome To Register Page</h1>";
+    });
+    $router->post('login', function(){
+        echo "Login Page";
+    });
+
+    /* Execute Routes */
+    $router->run();
